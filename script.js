fcde378f7673f769e1ac70179c411f24d8492786
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-29 16:00:36 +0200

    Types of Events and Event Handlers

--- script.js
+++ script.js
@@ -34,30 +34,18 @@ const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
 
 btnScrollTo.addEventListener("click", function (e) {
-    // const s1coords = section1.getBoundingClientRect();
-    // console.log(s1coords);
-
-    // console.log(e.target.getBoundingClientRect());
-
-    // console.log(`Current scroll (X/Y)`, window.pageXOffset, window.pageYOffset);
-
-    // console.log(
-    //     "height/width of viewport",
-    //     document.documentElement.clientHeight,
-    //     document.documentElement.clientWidth
-    // );
-
-    // Scrolling
-    // window.scrollTo(
-    //     s1coords.left + window.pageXOffset,
-    //     s1coords.top + window.pageYOffset
-    // );
-
-    // window.scrollTo({
-    //     left: s1coords.left + window.pageXOffset,
-    //     top: s1coords.top + window.pageYOffset,
-    //     behavior: "smooth",
-    // });
-
     section1.scrollIntoView({ behavior: "smooth" });
 });
+
+function alertH1(e) {
+    alert("addEventListener: Great! You are reading the heading :D");
+}
+
+const h1 = document.querySelector("h1");
+h1.addEventListener("mouseenter", alertH1);
+
+setTimeout(() => h1.removeEventListener("mouseenter", alertH1), 3000);
+
+// h1.onmouseenter = function (e) {
+//     alert("addEventListener: Great! You are reading the heading :D");
+// };
