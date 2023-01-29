6a523afbaa5afaab0068a0d2d6c6e94976e1fbf9
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-29 14:10:21 +0200

    Selecting, Creating, and Deleting Elements

--- script.js
+++ script.js
@@ -29,3 +29,40 @@ document.addEventListener("keydown", function (e) {
         closeModal();
     }
 });
+
+// Selecting elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector(".header");
+const allSections = document.querySelectorAll(".section");
+console.log(allSections);
+
+document.getElementById("section--1");
+const allButtons = document.getElementsByTagName("button");
+console.log(allButtons);
+
+console.log(document.getElementsByClassName("btn"));
+
+// Creating and inserting elements
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+message.textContent =
+    "We use cookies for improved functionality and analytics.";
+message.innerHTML = `We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it</button`;
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+// Delete elements
+document
+    .querySelector(".btn--close-cookie")
+    .addEventListener("click", function () {
+        // message.remove();
+        message.parentElement.removeChild(message);
+    });
