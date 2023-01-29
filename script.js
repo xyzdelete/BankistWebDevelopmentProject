6772103ef84593601b19258056e0755d4695e1cb
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-29 14:55:43 +0200

    Styles, Attributes and Classes

--- script.js
+++ script.js
@@ -66,3 +66,52 @@ document
         // message.remove();
         message.parentElement.removeChild(message);
     });
+
+// Styles
+message.style.backgroundColor = "#37383d";
+message.style.width = "120%";
+
+console.log(message.style.height);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+    Number.parseFloat(getComputedStyle(message).height, 10) + 40 + "px";
+
+document.documentElement.style.setProperty("--color-primary", "orangered");
+
+// Attributes
+const logo = document.querySelector(".nav__logo");
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+logo.alt = "Beutiful minimalist logo";
+console.log(logo.alt);
+
+// Non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute("designer"));
+logo.setAttribute("company", "Bankist");
+console.log(logo.getAttribute("company"));
+
+console.log(logo.src);
+console.log(logo.getAttribute("src"));
+
+const link = document.querySelector(".nav__link--btn");
+console.log(link.href);
+console.log(link.getAttribute("href"));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add("c");
+logo.classList.remove("c");
+logo.classList.toggle("c");
+logo.classList.contains("c"); // not includes
+
+// Don't use
+logo.className = "Arturs";
