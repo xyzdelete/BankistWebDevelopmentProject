b1997eebde9ae00743c16d5fec4da42b357c20a6
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-30 22:27:28 +0200

    Lazy Loading Images

--- script.js
+++ script.js
@@ -153,3 +153,30 @@ allSections.forEach(function (section) {
     sectionObserver.observe(section);
     section.classList.add("section--hidden");
 });
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll("img[data-src]");
+
+const loadImg = function (entries, observer) {
+    const [entry] = entries;
+    console.log(entry);
+
+    if (!entry.isIntersecting) return;
+
+    // Replace src with data-src
+    entry.target.src = entry.target.dataset.src;
+
+    entry.target.addEventListener("load", function () {
+        entry.target.classList.remove("lazy-img");
+    });
+
+    observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+    root: null,
+    threshold: 0,
+    rootMargin: "200px",
+});
+
+imgTargets.forEach((img) => imgObserver.observe(img));
