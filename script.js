d1fb77a4ea410b5de16ff54b49d8fce1627a96e6
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-30 12:49:04 +0200

    Event Propagation in Practice

--- script.js
+++ script.js
@@ -37,15 +37,32 @@ btnScrollTo.addEventListener("click", function (e) {
     section1.scrollIntoView({ behavior: "smooth" });
 });
 
-function alertH1(e) {
-    alert("addEventListener: Great! You are reading the heading :D");
-}
+// rgb(255, 255, 255)
+const randomInt = (min, max) =>
+    Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+    `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+console.log(randomColor(0, 255));
 
-const h1 = document.querySelector("h1");
-h1.addEventListener("mouseenter", alertH1);
+document.querySelector(".nav__link").addEventListener("click", function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log("LINK", e.target, e.currentTarget);
+    console.log(e.currentTarget === this);
 
-setTimeout(() => h1.removeEventListener("mouseenter", alertH1), 3000);
+    // Stop propagation
+    // e.stopPropagation();
+});
 
-// h1.onmouseenter = function (e) {
-//     alert("addEventListener: Great! You are reading the heading :D");
-// };
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log("CONTAINER", e.target, e.currentTarget);
+});
+
+document.querySelector(".nav").addEventListener(
+    "click",
+    function (e) {
+        this.style.backgroundColor = randomColor();
+        console.log("NAV", e.target, e.currentTarget);
+    },
+    false
+);
